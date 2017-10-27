a40bc1b6ff9f3cb7bca63789bf9bc0d5de1f3b2c
SauceCode1 <n9932798@qut.edu.au>
2017-10-27 12:13:45 +1000

    Shell - Step() Work in Progress

--- TankBattle/Shell.cs
+++ TankBattle/Shell.cs
@@ -11,23 +11,39 @@ namespace TankBattle
     {
 
         // THis is a test commit comment, please ignore and delete at some stage.
+        private float shellx;
+        private float shelly;
+        private float gravity;
+        private Shrapnel explosion;
+        private Opponent player;
+        private float xVelocity;
+        private float yVelocity;
 
         public Shell(float x, float y, float angle, float power, float gravity, Shrapnel explosion, Opponent player)
         {
-            //throw new NotImplementedException();
-
-
             float angleRadians = (90 - angle) * (float)Math.PI / 180;
 
             float magnitude = power / 50;
 
-            float xVelocity = (float)Math.Cos(angleRadians) * magnitude;
-            float yVelocity = (float)Math.Sin(angleRadians) * -magnitude;
+            xVelocity = (float)Math.Cos(angleRadians) * magnitude;
+            yVelocity = (float)Math.Sin(angleRadians) * -magnitude;
         }
 
         public override void Step()
         {
             throw new NotImplementedException();
+
+            shellx += xVelocity;
+            shelly += yVelocity;
+            shellx += protected_game.WindSpeed() / 1000.0f;
+            if (shellx > Battlefield.WIDTH || shellx < 0 || shelly > Battlefield.HEIGHT)
+            {
+                protected_game.CancelEffect(this);
+                return;
+            } else if (protected_game.CheckCollidedTank(shellx, shelly))
+            {
+                // NEED TO CALL ProjectileHit() FROM OPPONENT
+            }
         }
 
         public override void Render(Graphics graphics, Size size)
